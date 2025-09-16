8477aec008f585d3ba1109312c12b3871a5abe3a
ga5974 <gadsdena5974@student.faytechcc.edu>
2025-09-16 14:08:22 -0400

    Add files via upload

---
+++ P1LAB1_GadsdenAngel.py
@@ -0,0 +1,9 @@
+# Angel-Shanika Gadsden
+# 16 September 2025
+# P1LAB1
+# User enters first and last name, then is welcomed to CTI-110.
+
+firstname = input ("Enter your first name:")
+lastname = input ("Enter your last name:")
+
+print("Hello,", firstname, lastname +"!", "Welcome to CTI-110.")
